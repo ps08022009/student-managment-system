25dd58a69e42be03fd4c7b99c6df6c5aee8dba7b
Pranav Sankar <137827334+ps08022009@users.noreply.github.com>
2024-11-03 18:39:21 +0000

    enlarged sun icon

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -15,7 +15,7 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon" className="relative flex items-center justify-center w-10 h-10">
-          <Sun className="h-6 w-6 transition-transform duration-300 dark:rotate-90 dark:scale-0" />
+          <Sun className="h-8 w-8 transition-transform duration-300 dark:rotate-90 dark:scale-0" />
           <Moon className="absolute h-6 w-6 transition-transform duration-300 scale-0 dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
