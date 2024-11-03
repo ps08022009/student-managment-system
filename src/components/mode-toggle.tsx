ef156ef156ed3a652d5b9ce817381ac325ff0e5a
Pranav Sankar <137827334+ps08022009@users.noreply.github.com>
2024-11-03 18:36:59 +0000

    decompress sun image

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -14,9 +14,9 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-5 w-5 transition-transform duration-300 dark:scale-0 dark:rotate-90" />
-          <Moon className="absolute h-5 w-5 scale-0 transition-transform duration-300 dark:scale-100 dark:rotate-0" />
+        <Button variant="outline" size="icon" className="relative flex items-center justify-center w-10 h-10">
+          <Sun className="h-6 w-6 transition-transform duration-300 dark:rotate-90 dark:scale-0" />
+          <Moon className="absolute h-6 w-6 transition-transform duration-300 scale-0 dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
